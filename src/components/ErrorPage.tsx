f83d80774a9aa1dccab1ada7ed1c06d916296250
Sezer Doğru <sezer.dogru91@gmail.com>
2024-08-19 18:56:32 +0200

    fix:bug

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -3,19 +3,15 @@ import qrImg from "../../public/assets/qr.png";
 import { useLaunchParams } from "@telegram-apps/sdk-react";
 
 export function ErrorPage() {
-  let lp;
-
-  // Telegram Mini App içindeysek çalıştır
-  if (typeof window !== "undefined" && window.Telegram) {
-    lp = useLaunchParams();
-  }
+  const lp = useLaunchParams();
+  const isTelegramApp = typeof window !== "undefined" && window.Telegram;
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black">
       <span className="text-white text-4xl mb-4">Play on your mobile!</span>
       <Image src={qrImg} className="w-60" alt="" />
       <span className="text-white text-4xl my-4">XList</span>
-      {lp && (
+      {isTelegramApp && lp && (
         <span className="text-white text-xl mb-4">{`${lp.platform} ${lp.version}`}</span>
       )}
     </div>
