4eada17d54ee6feceec416986d10de2c5cf0adeb
Sezer Doğru <sezer.dogru91@gmail.com>
2024-08-19 19:07:16 +0200

    fix:bug

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
 import qrImg from "../../public/assets/qr.png";
+import { useEffect } from "react";
 import { useLaunchParams } from "@telegram-apps/sdk-react";
 
-export function ErrorPage() {
-  const lp = useLaunchParams();
-  const isTelegramApp = typeof window !== "undefined" && (window as any).Telegram;
+export function ErrorPage({
+  error,
+  reset,
+}: {
+  error?: Error & { digest?: string };
+  reset?: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black">
       <span className="text-white text-4xl mb-4">Play on your mobile!</span>
       <Image src={qrImg} className="w-60" alt="" />
       <span className="text-white text-4xl my-4">XList</span>
-      {isTelegramApp && lp && (
-        <span className="text-white text-xl mb-4">{`${lp.platform} ${lp.version}`}</span>
-      )}
     </div>
   );
 }
