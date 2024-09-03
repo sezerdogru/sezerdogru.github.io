d673413dc93b0f128f159459e7bb825d935fb382
Sezer Doğru <sezer.dogru91@gmail.com>
2024-09-03 19:55:25 +0200

    add images

---
+++ src/data/items.ts
@@ -0,0 +1,693 @@
+import tonSvg from "./_assets/ton.svg";
+import arteImg from "../../public/assets/arte.jpg";
+import yesImg from "../../public/assets/yes.jpg";
+import tapImg from "../../public/assets/tap.jpg";
+import pixelImg from "../../public/assets/pixel.jpg";
+import acki_nacki from "../../public/assets/acki_nacki.jpeg";
+import afterland from "../../public/assets/afterland.jpeg";
+import agent301 from "../../public/assets/agent301.jpeg";
+import akedo from "../../public/assets/akedo.jpeg";
+import arenagames from "../../public/assets/arenagames.jpeg";
+import arte from "../../public/assets/arte.jpg";
+import b from "../../public/assets/b.jpeg";
+import banana from "../../public/assets/banana.jpeg";
+import bird_ton from "../../public/assets/bird_ton.jpeg";
+import birds from "../../public/assets/birds.jpeg";
+import bitget from "../../public/assets/bitget.jpeg";
+import Blum from "../../public/assets/Blum.jpg";
+import bounty_bay from "../../public/assets/bounty_bay.jpeg";
+import bump from "../../public/assets/bump.jpeg";
+import bunny_blitz from "../../public/assets/bunny_blitz.jpeg";
+import cats from "../../public/assets/cats.jpeg";
+import cedex from "../../public/assets/cedex.jpeg";
+import cex_io from "../../public/assets/cex.io.jpeg";
+import chick_coop from "../../public/assets/chick_coop.jpeg";
+import city_holder from "../../public/assets/city_holder.jpeg";
+import clayton_game from "../../public/assets/clayton_game.jpeg";
+import coub from "../../public/assets/coub.jpeg";
+import crypto_rank from "../../public/assets/crypto_rank.jpeg";
+import cyber_finance from "../../public/assets/cyber_finance.jpeg";
+import decode_ur_seed from "../../public/assets/decode_ur_seed.jpeg";
+import dogs from "../../public/assets/dogs.jpeg";
+import dormint from "../../public/assets/dormint.jpeg";
+import dotcoin from "../../public/assets/dotcoin.jpeg";
+import dragonz_land from "../../public/assets/dragonz_land.jpeg";
+import drum from "../../public/assets/drum.jpeg";
+import ducks from "../../public/assets/ducks.jpeg";
+import egg_drop from "../../public/assets/egg_drop.jpeg";
+import eggo_quest from "../../public/assets/eggo_quest.jpeg";
+import empire_game from "../../public/assets/empire_game.jpeg";
+import fanton_fantasy_football from "../../public/assets/fanton_fantasy_football.jpeg";
+import fintopia_jpg from "../../public/assets/fintopia.jpg.jpeg";
+import gain from "../../public/assets/gain.jpeg";
+import gamee from "../../public/assets/gamee.jpeg";
+import gemz from "../../public/assets/gemz.jpeg";
+import grand_combat from "../../public/assets/grand_combat.jpeg";
+import graphdex from "../../public/assets/graph-dex.jpeg";
+import hash_cats from "../../public/assets/hash_cats.jpeg";
+import hexn_io from "../../public/assets/hexn.io.jpeg";
+import holdcoin from "../../public/assets/holdcoin.jpeg";
+import hot_wallet from "../../public/assets/hot_wallet.jpeg";
+import Iceberg from "../../public/assets/Iceberg.jpeg";
+import kuroro_ranch from "../../public/assets/kuroro_ranch.jpeg";
+import lost_dogs from "../../public/assets/lost_dogs.jpeg";
+import major from "../../public/assets/major.jpeg";
+import maneki_mnk from "../../public/assets/maneki_mnk.jpeg";
+import matchquest from "../../public/assets/matchquest.jpeg";
+import memefi from "../../public/assets/memefi.jpeg";
+import memeland from "../../public/assets/memeland.jpeg";
+import metaracing_game from "../../public/assets/metaracing_game.jpeg";
+import moon_land from "../../public/assets/moon_land.jpeg";
+import moonberg_ai from "../../public/assets/moonberg_ai.jpeg";
+import mozoai from "../../public/assets/mozoai.jpeg";
+import music_coin from "../../public/assets/music_coin.jpeg";
+import okx_racer from "../../public/assets/okx_racer.jpeg";
+import olycoin from "../../public/assets/olycoin.jpeg";
+import onus from "../../public/assets/onus.jpeg";
+import orbitonx from "../../public/assets/orbitonx.jpeg";
+import oxygen_miner from "../../public/assets/oxygen_miner.jpeg";
+import pigs from "../../public/assets/pigs.jpeg";
+import pixel from "../../public/assets/pixel.jpg";
+import pixeltab from "../../public/assets/pixeltab.jpeg";
+import pixie from "../../public/assets/pixie.jpeg";
+import pocketfi from "../../public/assets/pocketfi.jpeg";
+import qr from "../../public/assets/qr.png";
+import rocky_rabbit from "../../public/assets/rocky_rabbit.jpeg";
+import sauces from "../../public/assets/sauces.jpg";
+import secondlive from "../../public/assets/secondlive.jpeg";
+import seed_coin from "../../public/assets/seed_coin.jpeg";
+import sidekick_fans from "../../public/assets/sidekick.fans.jpeg";
+import spell_wallet from "../../public/assets/spell_wallet.jpeg";
+import sphynx from "../../public/assets/sphynx.jpeg";
+import spinnercoin from "../../public/assets/spinnercoin.jpeg";
+import steamify from "../../public/assets/steamify.jpeg";
+import swopin from "../../public/assets/swopin.jpeg";
+import tabizoo from "../../public/assets/tabizoo.jpeg";
+import tap from "../../public/assets/tap.jpg";
+import time_farm from "../../public/assets/time_farm.jpeg";
+import Tomarket from "../../public/assets/Tomarket.jpg";
+import ton_kombat from "../../public/assets/ton_kombat.jpeg";
+import tonfish from "../../public/assets/tonfish.jpeg";
+import treasure_tapper from "../../public/assets/treasure_tapper.jpeg";
+import vertus from "../../public/assets/vertus.jpeg";
+import vr_tap_victoria_r from "../../public/assets/vr_tap_victoria_r.jpeg";
+import w3bflıx from "../../public/assets/w3bflıx.jpeg";
+import wcoin from "../../public/assets/wcoin.jpeg";
+import wizzwoods from "../../public/assets/wizzwoods.jpeg";
+import X_empire_musk_empire from "../../public/assets/X_empire_musk_empire.jpeg";
+import xkucoin from "../../public/assets/xkucoin.jpeg";
+import yes_coin_yumify from "../../public/assets/yes_coin_yumify.jpeg";
+import yes_coin from "../../public/assets/yes_coin.jpeg";
+import yes from "../../public/assets/yes.jpg";
+import yescoin from "../../public/assets/yescoin.jpeg";
+import zavod from "../../public/assets/zavod.jpeg";
+import zencoin from "../../public/assets/zencoin.jpeg";
+
+import Image from "next/image";
+
+const items = [
+  {
+    id: 1,
+    name: "Tomarket",
+    img: Tomarket,
+    url: "https://t.me/Tomarket_ai_bot/app?startapp=0000qtKx",
+  },
+  {
+    id: 2,
+    name: "Fintopio",
+    img: fintopia_jpg,
+    url: "https://fintop.io/2s8YzRz3WH",
+  },
+  {
+    id: 3,
+    name: "Blum",
+    img: Blum,
+    url: "https://t.me/blum/app?startapp=ref_0y0yjy5hLN",
+  },
+  {
+    id: 4,
+    name: "Hot Wallet",
+    img: hot_wallet,
+    url: "https://t.me/herewalletbot/app?startapp=10419750",
+  },
+  {
+    id: 5,
+    name: "Village",
+    img: null,
+    url: "https://t.me/herewalletbot/app?startapp=10419750-village-178850",
+  },
+  {
+    id: 6,
+    name: "Dogs",
+    img: dogs,
+    url: "https://t.me/dogshouse_bot/join?startapp=bkvmadi2SOeJ71pYim1UBg",
+  },
+  {
+    id: 7,
+    name: "Bird Ton",
+    img: bird_ton,
+    url: "https://t.me/BIRDTonBot/app?startapp=1034241175",
+  },
+  {
+    id: 8,
+    name: "Okx Racer",
+    img: okx_racer,
+    url: "https://t.me/OKX_official_bot/OKX_Racer?startapp=linkCode_95594511",
+  },
+  {
+    id: 9,
+    name: "Bitget",
+    img: bitget,
+    url: "https://t.me/BitgetOfficialBot/Bitget?startapp=JwnaGhlngUX3oFNY1AWUBopuj7SF6JZ",
+  },
+  {
+    id: 10,
+    name: "Major",
+    img: major,
+    url: "https://t.me/major/start?startapp=1034241175",
+  },
+  {
+    id: 11,
+    name: "Spell Wallet",
+    img: spell_wallet,
+    url: "https://t.me/spell_wallet_bot/wallet?startapp=referral=sei1r7qrl7kt4lkek74cwhfg20854wg7tylmtxyu4t",
+  },
+  {
+    id: 12,
+    name: "Seed App",
+    img: seed_coin,
+    url: "t.me/seed_coin_bot/app?startapp=1034241175",
+  },
+  {
+    id: 13,
+    name: "Time Farm",
+    img: time_farm,
+    url: "https://t.me/TimeFarmCryptoBot?start=L62IE9FFjft2u9dX",
+  },
+  {
+    id: 14,
+    name: "Vertus",
+    img: vertus,
+    url: "https://t.me/vertus_app_bot/app?startapp=1034241175",
+  },
+  {
+    id: 15,
+    name: "Cyber Finance",
+    img: cyber_finance,
+    url: "https://t.me/CyberFinanceBot/game?startapp=cj1raENlaENub2MyZlgmdT1yZWY",
+  },
+  {
+    id: 16,
+    name: "Cex.io",
+    img: cex_io,
+    url: "https://t.me/cexio_tap_bot?start=1716712060363033",
+  },
+  {
+    id: 17,
+    name: "Second Live",
+    img: secondlive,
+    url: "https://t.me/Secondlive_bot/app?startapp=armnessh",
+  },
+  {
+    id: 18,
+    name: "PocketFi",
+    img: pocketfi,
+    url: "https://t.me/pocketfi_bot/Mining?startapp=1034241175",
+  },
+  {
+    id: 19,
+    name: "Pixie",
+    img: pixie,
+    url: "https://t.me/pixie_project_bot?start=1034241175",
+  },
+  {
+    id: 20,
+    name: "Arena Games",
+    img: arenagames,
+    url: "https://t.me/Arenavsbot?start=ref_nXztFZAqv39a8b9zhqQhx7",
+  },
+  {
+    id: 21,
+    name: "Zavod Wallet",
+    img: zavod,
+    url: "https://t.me/Mdaowalletbot?start=1034241175",
+  },
+  {
+    id: 22,
+    name: "Drum game",
+    img: drum,
+    url: "https://t.me/drumtap_bot?start=1716917987487359",
+  },
+  {
+    id: 23,
+    name: "Ton Fish",
+    img: tonfish,
+    url: "https://t.me/TONPET_premining_bot?start=Aeroaki",
+  },
+  {
+    id: 24,
+    name: "Iceberg",
+    img: Iceberg,
+    url: "https://t.me/IcebergAppBot?start=referral_1034241175",
+  },
+  {
+    id: 25,
+    name: "Dragonz Land",
+    img: dragonz_land,
+    url: "https://t.me/dragonz_land_bot/app?startapp=ref-667c2156e2069e47c98dd938",
+  },
+  {
+    id: 26,
+    name: "City Holder",
+    img: city_holder,
+    url: "https://t.me/cityholder/game?startapp=1034241175",
+  },
+  {
+    id: 27,
+    name: "Memefi",
+    img: memefi,
+    url: "https://t.me/memefi_coin_bot?start=r_e6819b1bac",
+  },
+  {
+    id: 28,
+    name: "Bounty Bay",
+    img: bounty_bay,
+    url: "https://t.me/bountybay_bot/deals?startapp=invite-3da54497",
+  },
+  {
+    id: 29,
+    name: "Hexn",
+    img: hexn_io,
+    url: "https://t.me/hexn_bot/app?startapp=9bd213c4-073f-49a3-b9cc-8eb67f4b3b09",
+  },
+  {
+    id: 30,
+    name: "Spinner Coin",
+    img: spinnercoin,
+    url: "https://t.me/SpinnerCoin_bot/app?startapp=r_215458",
+  },
+  {
+    id: 31,
+    name: "Musk Empire",
+    img: X_empire_musk_empire,
+    url: "https://t.me/empirebot/game?startapp=hero1034241175",
+  },
+  {
+    id: 32,
+    name: "Rocky Rabbit",
+    img: rocky_rabbit,
+    url: "https://t.me/rocky_rabbit_bot/play?startapp=frId1034241175",
+  },
+  {
+    id: 33,
+    name: "Chick Coop",
+    img: chick_coop,
+    url: "https://t.me/chickcoopofficial_bot/chickcoop?startapp=ref_1034241175",
+  },
+  {
+    id: 34,
+    name: "Cats",
+    img: cats,
+    url: "http://t.me/catsgang_bot/join?startapp=axEnI3YjOC2Z7BZCa44DN",
+  },
+  {
+    id: 35,
+    name: "Tabizoo",
+    img: tabizoo,
+    url: "https://t.me/tabizoobot/tabizoo?startapp=2aPksQ",
+  },
+  {
+    id: 36,
+    name: "Crypto Rank",
+    img: crypto_rank,
+    url: "https://t.me/cryptorank_app_bot/points?startapp=ref_1034241175_",
+  },
+  {
+    id: 37,
+    name: "YesCoin",
+    img: yes_coin,
+    url: "https://t.me/theYescoin_bot/Yescoin?startapp=lQbHm8",
+  },
+  {
+    id: 38,
+    name: "Yescoin 2",
+    img: yescoin,
+    url: "https://t.me/realyescoinbot/something?startapp=r_1034241175",
+  },
+  {
+    id: 39,
+    name: "Yescoin 3 (yumify)",
+    img: yes_coin_yumify,
+    url: "https://t.me/Yumify_Bot?start=1034241175",
+  },
+  {
+    id: 40,
+    name: "Dotcoin",
+    img: dotcoin,
+    url: "https://t.me/dotcoin_bot?start=r_1034241175",
+  },
+  {
+    id: 41,
+    name: "Hold Coin",
+    img: holdcoin,
+    url: "https://t.me/theHoldCoinBot/app?startapp=ref_1gCFFiBn",
+  },
+  {
+    id: 42,
+    name: "Pixelverse",
+    img: pixelImg,
+    url: "https://t.me/pixelversexyzbot?start=1034241175",
+  },
+  {
+    id: 43,
+    name: "MozoAI",
+    img: null,
+    url: "https://t.me/MozoAI_bot?start=XaafU7Bv",
+  },
+  {
+    id: 44,
+    name: "Treasure Tapper",
+    img: treasure_tapper,
+    url: "https://t.me/treasure_tapper_bot/start?startapp=invite-1034241175-1724328465969-invite",
+  },
+  {
+    id: 45,
+    name: "W coin",
+    img: wcoin,
+    url: "https://t.me/wcoin_tapbot?start=MTAzNDI0MTE3NQ",
+  },
+  {
+    id: 46,
+    name: "Bump",
+    img: bump,
+    url: "https://t.me/MMproBump_bot?start=ref_1034241175",
+  },
+  {
+    id: 47,
+    name: "Onus",
+    img: onus,
+    url: "https://t.me/onus_tap_tap_tap_bot/join?startapp=1721741410493",
+  },
+  {
+    id: 48,
+    name: "Match Quest",
+    img: matchquest,
+    url: "https://t.me/MatchQuestBot/start?startapp=0c378c83a599fa465409f7ee08d87d85",
+  },
+  {
+    id: 49,
+    name: "OrbitonX",
+    img: orbitonx,
+    url: "https://t.me/orbitonx_bot/orbitonx?startapp=friendId1034241175",
+  },
+  {
+    id: 50,
+    name: "Hold Wallet",
+    img: null,
+    url: "(Suspended)",
+  },
+  {
+    id: 51,
+    name: "Gemz",
+    img: gemz,
+    url: "https://t.me/gemzcoin_bOt/tap?startapp=17zZNH-UMDI5MW24KYouQWlE",
+  },
+  {
+    id: 52,
+    name: "Memeland",
+    img: null,
+    url: "https://t.me/metaland_bot/click?startapp=1034241175",
+  },
+  {
+    id: 53,
+    name: "Xkucoin",
+    img: null,
+    url: "https://t.me/xkucoinbot/kucoinminiapp?startapp=cm91dGU9JTJGdGFwLWdhbWUlM0ZpbnZpdGVyVXNlcklkJTNEMTAzNDI0MTE3NSUyNnJjb2RlJTNE",
+  },
+  {
+    id: 54,
+    name: "Dormint",
+    img: null,
+    url: "https://t.me/dormint_bot/dormint_bot?startapp=ref_R3X2Z6UGB6LUJJJ5AAC6",
+  },
+  {
+    id: 55,
+    name: "Banana",
+    img: null,
+    url: "https://t.me/OfficialBananaBot/banana?startapp=referral=N4HAQU",
+  },
+  {
+    id: 56,
+    name: "Swopin",
+    img: null,
+    url: "https://t.me/swopin_bot/swopin?startapp=1034241175",
+  },
+  {
+    id: 57,
+    name: "Gamee",
+    img: null,
+    url: "https://t.me/gamee/start?startapp=ref_1034241175",
+  },
+  {
+    id: 58,
+    name: "Oxygen Miner",
+    img: null,
+    url: "https://t.me/oxygenminerbot/app?startapp=1034241175",
+  },
+  {
+    id: 59,
+    name: "Empire",
+    img: null,
+    url: "https://t.me/empire_gamebot/app?startapp=ref_86600279684e11efa9520242ac1b0002cd7a9c",
+  },
+  {
+    id: 60,
+    name: "Agent 301",
+    img: null,
+    url: "https://t.me/Agent301Bot/app?startapp=onetime1034241175",
+  },
+  {
+    id: 61,
+    name: "Grand Combat",
+    img: null,
+    url: "https://t.me/grandcombat_bot/start?startapp=pTvk54pfOC",
+  },
+  {
+    id: 62,
+    name: "Lost Dogs",
+    img: null,
+    url: "https://t.me/lost_dogs_bot/lodoapp?startapp=ref-u_1034241175__s_664035",
+  },
+  {
+    id: 63,
+    name: "Olycoin",
+    img: null,
+    url: "https://t.me/OLYCoinBot/OLYCoin?startapp=FS26EA5",
+  },
+  {
+    id: 64,
+    name: "Ducks",
+    img: null,
+    url: "https://t.me/duckscoop_bot/app?startapp=ocT5N3VpfC",
+  },
+  {
+    id: 65,
+    name: "Pigs",
+    img: null,
+    url: "https://t.me/PigshouseBot?start=1034241175",
+  },
+  {
+    id: 66,
+    name: "Graph Dex",
+    img: null,
+    url: "https://t.me/graph_dex_bot?start=1034241175",
+  },
+  {
+    id: 67,
+    name: "Coub",
+    img: null,
+    url: "https://t.me/coub/app?startapp=coub__marker_18276156",
+  },
+  {
+    id: 68,
+    name: "Birds",
+    img: null,
+    url: "https://t.me/birdx2_bot/birdx?startapp=1034241175",
+  },
+  {
+    id: 69,
+    name: "Cedex",
+    img: null,
+    url: "https://t.me/cedex_tap_bot?start=1717782182139537",
+  },
+  {
+    id: 70,
+    name: "Meta Racing",
+    img: null,
+    url: "https://t.me/metaracinggame_bot/app?startapp=bonus_jia73q5i9e",
+  },
+  {
+    id: 71,
+    name: "W3BFLIX",
+    img: null,
+    url: "https://t.me/W3BFLIXBot?start=iv521de3808a",
+  },
+  {
+    id: 72,
+    name: "Sphynx",
+    img: null,
+    url: "t.me/sphynxmeme_bot/bless?startapp=wHv4JR2W",
+  },
+  {
+    id: 73,
+    name: "Decode Seed",
+    img: null,
+    url: "https://t.me/DecodeSeedBot?start=JU67L4O4",
+  },
+  {
+    id: 74,
+    name: "Eggo Quest",
+    img: null,
+    url: "https://t.me/eggo_quest_bot?start=1034241175",
+  },
+  {
+    id: 75,
+    name: "Maneki Neko",
+    img: null,
+    url: "https://t.me/MNK_Inc_bot/mnk?startapp=invite_Aeroaki",
+  },
+  {
+    id: 76,
+    name: "Egg Drop",
+    img: null,
+    url: "https://t.me/EggDrop_GombleBot/app?startapp=5552c667b87a41c78d47ae25c15ce591",
+  },
+  {
+    id: 77,
+    name: "Gm.ai",
+    img: null,
+    url: "https://t.me/gmdotaibot?profile",
+  },
+  {
+    id: 78,
+    name: "Gain",
+    img: null,
+    url: "https://t.me/The_Gain_Bot?start=SbqQ6pGR",
+  },
+  {
+    id: 79,
+    name: "Gala Music Miner",
+    img: null,
+    url: "https://t.me/GalaMusicMinerEarnTREZBot/start?startapp=invite-1034241175-1725212927679",
+  },
+  {
+    id: 80,
+    name: "Akedo Games",
+    img: null,
+    url: "https://t.me/Akedo_Bot/AkedogGenerator?startapp=42975442776663_1034241175",
+  },
+  {
+    id: 81,
+    name: "Steamify",
+    img: null,
+    url: "https://t.me/steamify_bot/app?startapp=4KBr4G",
+  },
+  {
+    id: 82,
+    name: "Ton Kombat",
+    img: null,
+    url: "https://t.me/Ton_kombat_bot/app?startapp=1034241175",
+  },
+  {
+    id: 83,
+    name: "B",
+    img: null,
+    url: "t.me/b_usersbot/join?startapp=ref-5JRub6JCVcDMUYEcqWGyKk",
+  },
+  {
+    id: 84,
+    name: "Moonberg",
+    img: null,
+    url: "https://t.me/moonbergai_bot/join?startapp=LeOkPZIWSb",
+  },
+  {
+    id: 85,
+    name: "Clayton",
+    img: null,
+    url: "https://t.me/claytoncoinbot/game?startapp=1034241175",
+  },
+  {
+    id: 86,
+    name: "Zencoin",
+    img: null,
+    url: "https://t.me/theZencoin_bot/zencoin?startapp=r=1034241175",
+  },
+  {
+    id: 87,
+    name: "Hash Cats",
+    img: null,
+    url: "https://t.me/hash_cats_bot?start=mSRJomspyW",
+  },
+  {
+    id: 88,
+    name: "Moon land",
+    img: null,
+    url: "https://t.me/moon_land_bot/to_the_moon?startapp=SS7IW14",
+  },
+  {
+    id: 89,
+    name: "After Land",
+    img: null,
+    url: "https://t.me/Afterland_Bot/app?startapp=aladn9",
+  },
+  {
+    id: 90,
+    name: "Bunny Blitz",
+    img: null,
+    url: "https://t.me/BunnyBlitz_bot?start=r_1034241175",
+  },
+  {
+    id: 91,
+    name: "Acki Nacki",
+    img: null,
+    url: "https://t.me/ackinacki_bot/ackinacki_app?startapp=eyJyZWZlcnJlciI6ImFlcm9ha2kifQ",
+  },
+  {
+    id: 92,
+    name: "Kuroro Ranch",
+    img: null,
+    url: "https://t.me/KuroroRanchBot/ranch?startapp=ref-5AA88554",
+  },
+  {
+    id: 93,
+    name: "Victoria R",
+    img: null,
+    url: "https://t.me/VRtapBot?start=Oh8BwsoFfc1KZv45",
+  },
+  {
+    id: 94,
+    name: "Wizzwoods",
+    img: null,
+    url: "https://t.me/WizzwoodsBot/app?startapp=rp_1086972",
+  },
+  {
+    id: 95,
+    name: "Sauces World",
+    img: null,
+    url: "https://sauces.world/@akimoto",
+  },
+  {
+    id: 96,
+    name: "Fanton Football",
+    img: null,
+    url: "https://t.me/FanTonGameBot/app?startapp=play_utm-ref_MYHGl2nTOwRS_o1nHJSizr8k",
+  },
+  {
+    id: 97,
+    name: "SideKick Fans",
+    img: null,
+    url: "https://t.me/sidekick_fans_bot?start=1034241175",
+  },
+];
+
+export { items };
