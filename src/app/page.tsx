010b49a04b9afb96c629bb433d66d7877253166a
Sezer Doğru <sezer.dogru91@gmail.com>
2024-09-04 07:24:17 +0200

    update:updated bgcolor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function InitDataPage() {
   }, [initData]);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="min-h-screen bg-black p-8">
       {/*<Link className="flex items-center mb-4" href="/ton-connect">
         <Image
           src={tonSvg.src}
@@ -32,33 +32,49 @@ export default function InitDataPage() {
           className="mr-2"
         />
 
-        <span className="text-black">{userRows?.username}</span>
+        <span className="text-white">{userRows?.username}</span>
       </div>
       {/*
-        <span className="text-black">{webApp?.platform}</span>
-        <span className="text-black">{webApp?.BiometricManager?.deviceId}</span>
+        <span className="text-white">{webApp?.platform}</span>
+        <span className="text-white">{webApp?.BiometricManager?.deviceId}</span>
         */}
-      <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">
+      <h1 className="text-4xl font-bold mb-8 text-center text-white">
         Our List - Remember to Play Every Day
       </h1>
-      <h2 className="text-black mb-4 font-bold">Best projects💲💎⬇️</h2>
+      <h2 className="text-white mb-4 font-bold">Best projects💲💎⬇️</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {items.map((item) => (
           <Link key={item.id} href={item?.url ?? "#"} passHref>
-            <div className="bg-white shadow-lg rounded-lg p-6 cursor-pointer hover:bg-gray-50 transition flex items-center justify-between">
-              <div className="flex items-center">
-                <Image
-                  src={
-                    item?.img ?? "https://web.telegram.org/z/icon-192x192.png"
-                  }
-                  alt=""
-                  width={50}
-                  height={50}
-                  className="mr-2"
-                />
-                <span className="text-xl font-semibold text-gray-900">
-                  {item.name}
-                </span>
+            <div className="bg-purple-500 shadow-lg rounded-lg p-6 cursor-pointer hover:bg-white transition">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <Image
+                    src={
+                      item?.img ?? "https://web.telegram.org/z/icon-192x192.png"
+                    }
+                    alt=""
+                    width={50}
+                    height={50}
+                    className="mr-2"
+                  />
+                  <span className="text-xl font-semibold text-gray-900">
+                    {item.name}
+                  </span>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6 text-yellow-300"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
+                </svg>
               </div>
             </div>
           </Link>
